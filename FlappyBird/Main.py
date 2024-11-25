8d56fe7f5f2dc7988dff463b03c3a0dc1ea8d0a2
Luke-Danley <ldanley422@lsr7.net>
2024-11-25 17:14:04 -0600

    restarted code

--- FlappyBird/Main.py
+++ FlappyBird/Main.py
@@ -1,42 +1,8 @@
-import pygame
-
-from pygame.locals import(
-    RLEACCEL,
-    K_UP,
-    K_DOWN,
-    KEYDOWN,
-    QUIT,
-)
-
-pygame.init()
-
-Screen_Width = 1200
-Screen_Height = 800
+#import pygame
 
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        super(Player, self).__init__()
-        self.surf = pygame.image.load("bob.jpg").convert()
-        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
-        self.rect = self.surf.get_rect()
-
-        
-screen = pygame.display.set_mode((Screen_Width, Screen_Height))
+#intialize pygame
 
 
-player = Player()
-all_sprites = pygame.sprite.Group()
-all_sprites.add(player)
+#create a screen
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            running = False
-    
-    screen.fill((135, 206, 250))
-    all_sprites.draw(screen)
-    pygame.display.flip()
-
-pygame.quit()
