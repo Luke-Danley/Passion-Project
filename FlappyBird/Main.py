d3698adb77e63b4c58942825b371392130736ca5
Luke-Danley <ldanley422@lsr7.net>
2024-11-25 19:05:05 -0600

    created background for flappy bird

--- FlappyBird/Main.py
+++ FlappyBird/Main.py
@@ -1,8 +1,41 @@
 #import pygame
-
+import pygame
 
 #intialize pygame
+pygame.init()
 
+#import keystrokes
+from pygame.locals import(
+    RLEACCEL,
+    K_UP,
+    K_DOWN,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
 
 #create a screen
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
+#set background image
+background = pygame.image.load('FlappyBird/FlappyBackground.png')
+
+#create game loop
+running = True
+
+while running:
+    for event in pygame.event.get():
+        #close the game if the user hits escape or closes the window
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+        elif event.type == QUIT:
+            running = False
+    
+    #put the background on the screen
+    screen.blit(background, (0, 0))
+
+    #update display
+    pygame.display.flip()    
